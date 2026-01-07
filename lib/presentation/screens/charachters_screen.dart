cd2505a5cf4acb08c82fce7dbd3b54b0346c9d66
RonydaEssam <ronyda988@gmail.com>
2026-01-07 20:32:10 +0200

    prepare UI widgets for showing grid view list

--- lib/presentation/screens/charachters_screen.dart
+++ lib/presentation/screens/charachters_screen.dart
@@ -1,13 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_app/business_logic/cubit/characters_cubit.dart';
+import 'package:my_app/constants/app_colors.dart';
+import 'package:my_app/data/models/character.dart';
+import 'package:my_app/presentation/widgets/character_item.dart';
 
-class CharactersScreen extends StatelessWidget {
+class CharactersScreen extends StatefulWidget {
   const CharactersScreen({super.key});
+
+  @override
+  State<CharactersScreen> createState() => _CharactersScreenState();
+}
+
+class _CharactersScreenState extends State<CharactersScreen> {
+  late List<Character> allCharacters;
+
+  @override
+  void initState() {
+    super.initState();
+    allCharacters = BlocProvider.of<CharactersCubit>(
+      context,
+    ).getAllCharacters();
+  }
+
+  Widget buildBlocWidget() {
+    return BlocBuilder<CharactersCubit, CharactersState>(
+      builder: (context, state) {
+        if (state is CharactersLoaded) {
+          allCharacters = (state).characters;
+          return buildLoadedListWidget();
+        } else {
+          return showLoadingIndicator();
+        }
+      },
+    );
+  }
+
+  Widget showLoadingIndicator() {
+    return Center(
+      child: CircularProgressIndicator(color: AppColors.bluish),
+    );
+  }
+
+  Widget buildLoadedListWidget() {
+    return SingleChildScrollView(
+      child: Container(
+        color: AppColors.grey,
+        child: Column(
+          children: [buildCharactersList()],
+        ),
+      ),
+    );
+  }
+
+  Widget buildCharactersList() {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 2 / 3,
+        crossAxisSpacing: 1,
+        mainAxisSpacing: 1,
+      ),
+      shrinkWrap: true,
+      physics: const ClampingScrollPhysics(),
+      padding: EdgeInsets.zero,
+      itemBuilder: (context, index) {
+        return CharacterItem();
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Rick and Morty characters')),
-      body: Text('Characters'),
+      appBar: AppBar(
+        backgroundColor: AppColors.bluish,
+        title: Text(
+          'Characters',
+          style: TextStyle(
+            color: AppColors.grey,
+            fontWeight: FontWeight.w500,
+            fontSize: 24,
+          ),
+        ),
+      ),
+      body: buildBlocWidget(),
     );
   }
 }
